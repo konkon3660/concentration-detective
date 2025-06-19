d7be145d6b9b8554a456a0501cd9f3d0a2f7150e
Junseo Kim <jookpower1022@gmail.com>
2025-06-19 15:21:03 +0900

    repec actuators, sensor

--- model/actuators.py
+++ model/actuators.py
@@ -1,16 +1,15 @@
-# led, 부저 출력 처리
+# actuators.py
 import RPi.GPIO as GPIO
 
-# 핀 설정
 LED_PIN = 22
 BUZZER_PIN = 23
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 GPIO.setup(BUZZER_PIN, GPIO.OUT)
-GPIO.setwarnings(False)
 
-pwm = GPIO.PWM(BUZZER_PIN, 262)
+# 부저 PWM 객체
+pwm = GPIO.PWM(BUZZER_PIN, 262)  # 도 음 높이
 
 def led_on():
     GPIO.output(LED_PIN, True)
@@ -19,7 +18,7 @@ def led_off():
     GPIO.output(LED_PIN, False)
 
 def buzzer_on():
-    pwm.start(50.0)
+    pwm.start(50.0)  # duty cycle 50%
 
 def buzzer_off():
-    pwm.start(50.0)
+    pwm.stop()
--- model/sensors.py
+++ model/sensors.py
@@ -1,17 +1,26 @@
-#조도, PIR 입력 처리
+# sensors.py
 import RPi.GPIO as GPIO
-import time
 
-# 핀 설정 (변수로 구성하여 쉽게 변경 가능)
+# 핀 번호 정의
 PIR_PIN = 17
-LDR_PIN = 27
+LDR_PIN = 27  # 추후 MCP3008으로 교체 시 여기에서 제거
 
+# GPIO 설정
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR_PIN, GPIO.IN)
 GPIO.setup(LDR_PIN, GPIO.IN)
 
-def is_motion_detected():
+def get_pir_value():
+    """PIR 센서 값 반환 (0 또는 1)"""
     return GPIO.input(PIR_PIN)
 
+def is_motion_detected():
+    return get_pir_value() == 1
+
+def get_ldr_value():
+    """조도 센서 디지털 값 반환"""
+    return GPIO.input(LDR_PIN)
+
 def is_dark():
-    return GPIO.input(LDR_PIN) == 0  # 0이면 어두운 것으로 간주
+    """0이면 어두운 것으로 간주"""
+    return get_ldr_value() == 0
